c86e2ba26444ac52c6ca566c3f7c05fe04e3fd7e
darcikole <53584516+darcikole@users.noreply.github.com>
2019-08-27 12:11:53 -0700

    step63

--- Basic_JavaScript1.js
+++ Basic_JavaScript1.js
@@ -4,16 +4,17 @@ document.write("Hello, World!");
 
 var J = "This is a JavaScript variable put in action";
 
-document.write(J);
+document.write(J); 
 
 var Q = "\“when you don't create things,"+ " you become defined by your tastes rather than ability." + " Your tastes only narrow & exclude people. So create.\”";
 
 document.write(Q);
 
-var wine1 = "Wines", cab = "Cadence", sparkling ="Dom Perignon", white = "De Lille"; 
+var wine1 = "Wine", cab = "Cadence", sparkling ="Dom Perignon", white = "De Lille"; 
 var cab = cab.fontcolor("red");
 var sparkling = sparkling.fontcolor("yellow");
 var white = white.fontcolor("black");
 document.write(cab);
 
 document.write(750/5);
+
---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,9 @@
+function step60() {
+   document.getElementById("learning").innerHTML = "He who would learn to fly one day must first learn to stand and walk and run and climb and dance; one cannot fly into flying. - Friedrich Nietzsche";
+}
+
+function step63() {
+    var sentence = "I am learning a new operator";
+    sentence += " and I hope to learn more and more!";
+    document.getElementById("Concantenate").innerHTML = sentence;
+}
